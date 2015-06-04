e7c1f1e7f0697f804ffc3b01017de15ccafa5798
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-06-04 12:02:51 -0700

    Scratch space for Swift review

---
+++ facts-ios/facts-ios-Scratchspace.playground/Contents.swift
@@ -0,0 +1,64 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Swift Recap
+
+var someVariable = "aVariable"
+
+
+let someConstant = 20
+
+someVariable = "anotherString"
+
+//var fruitsArray: [String] = ["apples"]
+var fruitsArray = ["apples"]
+fruitsArray.append("bananas")
+
+fruitsArray
+
+//Optionals
+
+var optionalString: String? = "hello"
+
+optionalString = nil
+
+println("\(someVariable)")
+
+
+//Functions
+
+func isDivisible(#dividend: Int, #divisor: Int) -> Bool {
+    if dividend % divisor == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+isDivisible(dividend: 2,divisor: 2)
+isDivisible(dividend: 1, divisor: 2)
+
+func isNotDivisible(#dividend: Int, #divisor: Int) -> Bool {
+    return !isDivisible(dividend: dividend, divisor: divisor)
+}
+
+isNotDivisible(dividend: 2,divisor: 2)
+isNotDivisible(dividend: 1, divisor: 2)
+
+//Tuples
+
+func greeting(person: String) -> (String,String) {
+    let language = "English"
+    let greeting = "Hello \(person)"
+    let v = (greeting, language)
+    return v
+}
+
+let h = greeting("Ryan")
+
+h.0
+h.1
+
